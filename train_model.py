b44f60369f89c9ad211d7b1d88b3b620e0abc95b
samuel sonawane <sosam29@gmail.com>
2019-03-17 21:35:00 -0400

    adding train model

--- train_model.py
+++ train_model.py
@@ -1,1 +1,10 @@
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.preprocessing.image import img_to_array
+from keras.optimizers import SGD
+import preprocess
+import numpy as np 
+import cv2
+import os
 
